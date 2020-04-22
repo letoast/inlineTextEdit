46dabd9423ce8ee1e5e4abf62eab8bd958fc98a6
letoast <ciroman@gmail.com>
2020-04-22 17:21:02 +0200

    first commit

---
+++ build/precache-manifest.401400fdfd2e9f565f14e07ceea4f287.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3a41c9fff3fd2ee8147d2809683e2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1c721bb35d01112dafd",
+    "url": "/static/css/main.c8c1d4d6.chunk.css"
+  },
+  {
+    "revision": "ef3959b783e157bc19f1",
+    "url": "/static/js/2.2185b4a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2185b4a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c721bb35d01112dafd",
+    "url": "/static/js/main.c4ab2c8f.chunk.js"
+  },
+  {
+    "revision": "e20e26f761fd51695877",
+    "url": "/static/js/runtime-main.267c4c41.js"
+  }
+]);
